11dd0022c1e1f30341083a09ca29268b87ec4f30
Nathan Falkoff <natefalkoff@gmail.com>
2017-10-27 13:05:51 -0400

    afternoon cleanup

---
+++ m3-w9d3-csharp-capstone/Capstone.Web.Tests/UnitTests/ParkWeatherSqlDALTest.cs
@@ -0,0 +1,31 @@
+﻿using Capstone.Web.Dal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.Tests.UnitTests
+{
+    class ParkWeatherSqlDALTest
+    {
+        string connectionString = @"Data Source=localhost\sqlexpress;Initial Catalog=ParkWeather;Integrated Security=True";
+        [TestMethod]
+        public void SurveyTest()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                CampGroundDAL campGroundDAL = new CampGroundDAL(connectionString);
+
+                List<CampGround> camps = campGroundDAL.GetCampground();
+
+                Assert.AreEqual(7, camps.Count);
+            }
+        }
+    }
+
+}
