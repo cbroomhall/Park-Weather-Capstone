df11e7175068ea2cee695f4b1aecc3cb4b76290e
Nathan Falkoff <natefalkoff@gmail.com>
2017-10-31 13:19:26 -0400

    10/31

--- m3-w9d3-csharp-capstone/Capstone.Web.Tests/UnitTests/ParkWeatherSqlDALTest.cs
+++ m3-w9d3-csharp-capstone/Capstone.Web.Tests/UnitTests/ParkWeatherSqlDALTest.cs
@@ -1,4 +1,6 @@
 ﻿using Capstone.Web.Dal;
+using Capstone.Web;
+using Capstone.Web.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
@@ -8,24 +10,70 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Capstone.Web.Tests.UnitTests
-{
-    class ParkWeatherSqlDALTest
+{   [TestClass]
+    public class ParkWeatherSqlDALTest
     {
         string connectionString = @"Data Source=localhost\sqlexpress;Initial Catalog=ParkWeather;Integrated Security=True";
         [TestMethod]
-        public void SurveyTest()
+        public void GetAllParksTest()
         {
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 conn.Open();
 
-                CampGroundDAL campGroundDAL = new CampGroundDAL(connectionString);
+                ParkWeatherSqlDal ParkWeatherDAL = new ParkWeatherSqlDal(connectionString);
 
-                List<CampGround> camps = campGroundDAL.GetCampground();
+                List<ParkModel> Parks = ParkWeatherDAL.GetAllParks();
 
-                Assert.AreEqual(7, camps.Count);
+                Assert.AreEqual(10, Parks.Count);
             }
         }
+
+        [TestMethod]
+        public void GetSingleParkTest()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                ParkWeatherSqlDal ParkWeatherDAL = new ParkWeatherSqlDal(connectionString);
+
+                ParkModel Park = ParkWeatherDAL.GetSinglePark("CVNP");
+
+                Assert.AreEqual("Cuyahoga Valley National Park", Park.ParkName);
+            }
+        }
+
+        [TestMethod]
+        public void GetForecastTest()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                ParkWeatherSqlDal ParkWeatherDAL = new ParkWeatherSqlDal(connectionString);
+
+                List<WeatherModel> Weather  = ParkWeatherDAL.GetForecast("CVNP");
+
+                Assert.AreEqual(38, Weather[0].Low);
+            }
+        }
+
+        //[TestMethod]
+        //public void GetSurveyResults()
+        //{
+        //    using (SqlConnection conn = new SqlConnection(connectionString))
+        //    {
+        //        conn.Open();
+
+        //        ParkWeatherSqlDal ParkWeatherDAL = new ParkWeatherSqlDal(connectionString);
+
+        //        List<SurveyModel> Surveys = ParkWeatherDAL.GetSurveyResults();
+
+        //        Assert.AreEqual(38, Weather[0].Low);
+        //    }
+
+        //}
     }
 
 }
